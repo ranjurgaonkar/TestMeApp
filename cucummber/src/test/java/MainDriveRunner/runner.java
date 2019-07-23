ceca98fa89a1a47c8c22044a33f2420227e39dcd
Training_B6E.00.13 <Training_B6E.00.13@BDC6-DX-87GNX52.dir.svc.accenture.com>
2019-07-23 10:53:14 +0530

    my fst commit

---
+++ cucummber/src/test/java/MainDriveRunner/runner.java
@@ -0,0 +1,20 @@
+package MainDriveRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		features = "Features",
+		glue = {"stepdefinition"}
+		
+		
+		)
+public class runner {
+
+}
+
+
---
+++ cucummber/src/test/java/stepdefinition/stepdefintion.java
@@ -0,0 +1,14 @@
+package stepdefinition;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+
+public class stepdefintion {
+	
+@When("^Larry increase the headphone quantity by 2$")
+	  public void Larry_increase_the_headphone_quantity_by_2()
+	  {
+	    System.out.println("Test is passed");
+	  }
+
+}
